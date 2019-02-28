0f477e9c2fc51d6743833bbfac55dbb3fa650700
Andrew <andrewpeliza@hotmail.com>
2019-02-28 21:37:39 +0000

    Updates

---
+++ precache-manifest.b72f20517e962f59ceecd669723a583d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb48cf2f110bfcf2b1f1",
+    "url": "/React-Dev-Tech-Test/static/css/main.2a1f75ac.chunk.css"
+  },
+  {
+    "revision": "cb48cf2f110bfcf2b1f1",
+    "url": "/React-Dev-Tech-Test/static/js/main.cb48cf2f.chunk.js"
+  },
+  {
+    "revision": "05224b2eb9abb9f33e22",
+    "url": "/React-Dev-Tech-Test/static/js/runtime~main.05224b2e.js"
+  },
+  {
+    "revision": "31e1a973e9a0260cc554",
+    "url": "/React-Dev-Tech-Test/static/js/2.31e1a973.chunk.js"
+  },
+  {
+    "revision": "32132be792dc8f7b28a1fd0fc98397ff",
+    "url": "/React-Dev-Tech-Test/static/media/shape.32132be7.svg"
+  },
+  {
+    "revision": "c67a9bc09c3198d8b56ebd67e6240e74",
+    "url": "/React-Dev-Tech-Test/index.html"
+  }
+];
